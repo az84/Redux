0808651d24b2f99c5aeb7d6950ecf465cb632c1e
azaidi <zaidialina1@gmail.com>
2021-11-12 21:26:21 -0500

    changes and updates

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,7 +1,6 @@
 import {reduxStore} from 'redux';
-import {DevTools} from 'redux-devtools-extension';
 import reducer from './reducers';
 
-const store = reduxStore(reducer, DevTools)
+const store = reduxStore(reducer)
 ;
 export default store;
